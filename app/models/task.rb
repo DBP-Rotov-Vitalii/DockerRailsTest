38b2d8eeda3123de4e84fa34c01b3674755906f2
Rotov Vitalii <vitaly.rotov@dualbootpartners.com>
2022-10-05 14:16:32 -0300

    added task state changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,33 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
   validates :author, presence: true
+
+  state_machine initial: :new_task do
+    event :to_develompment do
+      transition new_task: :in_development,
+                 in_qa: :in_development,
+                 in_code_rewiev: :in_development
+    end
+
+    event :to_archived do
+      transition new_task: :archived,
+                 released: :archived
+    end
+
+    event :to_qa do
+      transition in_development: :in_qa
+    end
+
+    event :to_code_rewiev do
+      transition in_qa: :in_code_rewiev
+    end
+
+    event :to_ready_for_release do
+      transition in_code_rewiev: :ready_for_release
+    end
+
+    event :to_released do
+      transition ready_for_release: :released
+    end
+  end
 end
